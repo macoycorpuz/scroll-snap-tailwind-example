e483d483476f410a496d22de7233a07085f7ea80
Marcuz Corpuz <macoycorpuz@gmail.com>
2021-08-14 04:01:31 +0900

    tailwind installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>Hello World</div>
-  );
+class App extends React.Component<{}> {
+  render() {
+    return (
+      <div className="h-screen bg-gray-500">
+        <section>
+          <h1>Page One</h1>
+        </section>
+        <section>
+          <h1>Page Two</h1>
+        </section>
+        <section>
+          <h1>Page Three</h1>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
